24e31ad2f6de68c3fde7a84a587c4f6c49160e50
Beha <shacknetisp@mail.com>
2018-10-16 11:30:40 -0400

    Implement.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fertilizer
+Fertilizer will slowly decay. Place absorbing pots on the same vertical level. If there is a flora node (like a flower) on any of the pots, there is a chance it will be fertilized and spread to another of the pots adjacent to the same block of fertilizer.
---
+++ init.lua
@@ -0,0 +1,103 @@
+local num_fertilizer_nodes = 10
+local cycle_time = 3 * 60
+local variation = 1 * 60
+
+for i=1,num_fertilizer_nodes do
+    local name = "fertilepots:fertilizer_" .. i
+    local next_name = (i ~= num_fertilizer_nodes) and ("fertilepots:fertilizer_" .. (i + 1)) or "air"
+    minetest.register_node(name, {
+        description = "Fertilizer",
+        drawtype = "nodebox",
+        node_box = {
+            type = "fixed",
+            fixed = {
+                {-0.5, -0.5, -0.5, 0.5, 0.5 - (0.1 * (i - 1)), 0.5},
+            },
+        },
+        tiles = {"default_dirt.png^[colorize:#333:127"},
+
+        groups = {fertilizer = 1, not_in_creative_inventory = (i == 1) and 0 or 1, crumbly = 3},
+        drop = (i == 1) and "fertilepots:fertilizer_1" or "",
+
+        on_construct = function(pos)
+            minetest.get_node_timer(pos):start(math.max(1, math.random(cycle_time - variation / 2, cycle_time + variation / 2) / num_fertilizer_nodes))
+        end,
+
+        on_timer = function(pos)
+            minetest.set_node(pos, {name = next_name})
+
+            if math.random() < 0.2 then
+                local empty = {}
+                local flora = {}
+
+                for x=-1,1 do
+                for z=-1,1 do
+                    local check = vector.add(pos, vector.new(x, 0, z))
+                    local above = vector.add(check, vector.new(0, 1, 0))
+
+                    if minetest.get_item_group(minetest.get_node(check).name, "flora_pot") > 0 then
+                        if minetest.get_item_group(minetest.get_node(above).name, "flora") > 0 then
+                            table.insert(flora, above)
+                        elseif minetest.get_node(above).name == "air" then
+                            table.insert(empty, above)
+                        end
+                    end
+                end
+                end
+
+                if #flora > 0 and #empty > 0 then
+                    minetest.set_node(empty[math.random(#empty)], {
+                        name = minetest.get_node(flora[math.random(#flora)]).name})
+                end
+            end
+        end,
+    })
+end
+
+minetest.register_craft{
+    output = "fertilepots:fertilizer_1",
+    recipe = {
+        {"group:sapling", "", "group:sapling"},
+        {"", "group:soil", ""},
+        {"group:sapling", "", "group:sapling"},
+    },
+}
+
+minetest.register_craft{
+    output = "fertilepots:fertilizer_1",
+    recipe = {
+        {"group:leaves", "group:leaves", "group:leaves"},
+        {"group:leaves", "group:soil", "group:leaves"},
+        {"group:leaves", "group:leaves", "group:leaves"},
+    },
+}
+
+minetest.register_node("fertilepots:pot", {
+    description = "Fertilizer Absorbing Pot",
+    groups = {flora_pot = 1, cracky = 3, oddly_breakable_by_hand = 3},
+    tiles = {
+        "default_dirt.png",
+        "default_brick.png",
+        "default_brick.png",
+        "default_brick.png",
+        "default_brick.png",
+        "default_brick.png",
+    },
+    drawtype = "nodebox",
+    node_box = {
+        type = "fixed",
+        fixed = {
+            {-0.5, 0, -0.5, 0.5, 0.5, 0.5},
+            {-0.35, -0.5, -0.35, 0.35, 0, 0.35},
+        },
+    },
+})
+
+minetest.register_craft{
+    output = "fertilepots:pot",
+    recipe = {
+        {"default:brick", "group:soil", "default:brick"},
+        {"default:brick", "group:soil", "default:brick"},
+        {"", "default:brick", ""},
+    },
+}
